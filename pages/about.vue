229880976fa4e420cb7f9f1ca56ee3c709ec566e
Raiu1210 <baio1484@gmail.com>
2021-02-11 16:23:49 +0900

    displayed article content

--- pages/about.vue
+++ pages/about.vue
@@ -1,39 +1,39 @@
 <template>
   <v-row>
-          <v-col
-            cols="12"
-            sm="2"
-          >
-            <v-sheet
-              rounded="lg"
-              min-height="268"
-            >
-              <!--  -->
-            </v-sheet>
-          </v-col>
+    <v-col
+      cols="12"
+      sm="1"
+    >
+      <v-sheet
+        rounded="lg"
+        min-height="268"
+      >
+        <!--  -->
+      </v-sheet>
+    </v-col>
 
-          <v-col
-            cols="12"
-            sm="8"
-          >
-            <v-sheet
-              min-height="70vh"
-              rounded="lg"
-            >
-              <!--  -->
-            </v-sheet>
-          </v-col>
+    <v-col
+      cols="12"
+      sm="9"
+    >
+      <v-sheet
+        min-height="70vh"
+        rounded="lg"
+      >
+        <!--  -->
+      </v-sheet>
+    </v-col>
 
-          <v-col
-            cols="12"
-            sm="2"
-          >
-            <v-sheet
-              rounded="lg"
-              min-height="268"
-            >
-              <!--  -->
-            </v-sheet>
-          </v-col>
-        </v-row>
+    <v-col
+      cols="12"
+      sm="3"
+    >
+      <v-sheet
+        rounded="lg"
+        min-height="268"
+      >
+        <!--  -->
+      </v-sheet>
+    </v-col>
+  </v-row>
 </template>
--- pages/article/_article_id.vue
+++ pages/article/_article_id.vue
@@ -1,8 +1,47 @@
 <template>
-  <div>
-    <h1>{{this.$route.params.article_id}}</h1>
-  </div>
+  <v-row>
+    <v-col
+      cols="12"
+      sm="1"
+    >
+      <v-sheet
+        rounded="lg"
+        min-height="268"
+      >
+        <!--  -->
+      </v-sheet>
+    </v-col>
+
+    <v-col
+      cols="12"
+      sm="9"
+    >
+      <v-sheet
+        min-height="70vh"
+        rounded="lg"
+      >
+        <!-- コンテンツ表示 -->
+        <!-- {{article}} -->
+        <h1>{{title}}</h1>
+        <div v-html="$md.render(content)"></div>
+        <p>authorAddress</p>
+      </v-sheet>
+    </v-col>
+
+    <v-col
+      cols="12"
+      sm="2"
+    >
+      <v-sheet
+        rounded="lg"
+        min-height="268"
+      >
+
+      </v-sheet>
+    </v-col>
+  </v-row>
 </template>
+
 
 
 <script>
@@ -11,7 +50,11 @@ import {axiosInstance as Api} from '~/myModules/api'
 export default {
   data(){
     return {
-
+      article: null,
+      title: '',
+      content: '',
+      created: '',
+      authorAddress: ''
     }
   },
   async created() {
@@ -21,7 +64,11 @@ export default {
       }
     })
 
-    console.log(res)
+    this.article = res['data']
+    this.title = res['data']['title']
+    this.content = res['data']['content']
+    this.created = res['data']['createdAt']
+    this.authorAddress = res['data']['user']['address']
   }
 }
 </script>
