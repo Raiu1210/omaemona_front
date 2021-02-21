1314211a77da35534999dc20713e53149f78dd89
Raiu1210 <baio1484@gmail.com>
2021-02-21 21:20:24 +0900

    test 7

--- nuxt.config.js
+++ nuxt.config.js
@@ -27,19 +27,20 @@ export default {
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       // meta info
       // { hid: 'description', name: 'description', content: '記事を書いてMONAがもらえる！気に入った記事にMONAが送れる！' },
-      // { hid: 'og:site_name', property: 'og:site_name', content: 'オマエモナー' },
-      // { hid: 'og:type', property: 'og:type', content: 'article' },
-      { property: 'og:url', content: 'https://omaemona.info/article/3' },
       { property: 'og:title', content: 'オマエモナー' },
-      { ogproperty: 'og:description', content: '記事を書いてMONAがもらえる！気に入った記事にMONAが送れる！' },
       { property: 'og:image', content: 'https://omaemona.info:8443/ogp/default.png' },
+      { property: 'og:description', content: '記事を書いてMONAがもらえる！気に入った記事にMONAが送れる！' },
+      { property: 'og:url', content: 'https://omaemona.info/article/3' },
+      { property: 'og:type', content: 'article' },
+      { property: 'og:site_name', content: 'オマエモナー' },
+
       // for twitter
       // { hid: 'twitter:creator', name: 'twitter:creator', content: '@Mr_1484'},
-      { name: 'twitter:card', content: 'summary_large_image' },
       { name: 'twitter:site', content: '@Mr_1484' },
-      // { hid: 'twitter:title', name: 'twitter:title', content: 'オマエモナー' },
-      // { hid: 'twitter:description', name: 'twitter:description', content: '記事を書いてMONAがもらえる！気に入った記事にMONAが送れる！'},
-      // { hid: 'twitter:image', name: 'twitter:image', content: 'https://omaemona.info:8443/ogp/default.png'},
+      { name: 'twitter:card', content: 'summary_large_image' },
+      { name: 'twitter:title', content: 'オマエモナー' },
+      { name: 'twitter:image', content: 'https://omaemona.info:8443/ogp/default.png'},
+      { name: 'twitter:description', content: '記事を書いてMONAがもらえる！気に入った記事にMONAが送れる！'},
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
