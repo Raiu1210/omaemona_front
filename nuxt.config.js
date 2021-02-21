0f522351eb2568717b293ff4fb0323736742ecad
Raiu1210 <baio1484@gmail.com>
2021-02-21 20:00:01 +0900

    ogp try 4

--- nuxt.config.js
+++ nuxt.config.js
@@ -22,19 +22,22 @@ export default {
       lang: 'ja'
     },
     meta: [
+      // setting
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '記事を書いてMONAがもらえる！気に入った記事にMONAが送れる！' },
+      // meta info
+      // { hid: 'description', name: 'description', content: '記事を書いてMONAがもらえる！気に入った記事にMONAが送れる！' },
       { hid: 'og:site_name', property: 'og:site_name', content: 'オマエモナー' },
-      { hid: 'og:type', property: 'og:type', content: 'website' },
-      { hid: 'og:url', property: 'og:url', content: 'https://omaemona.info' },
+      { hid: 'og:type', property: 'og:type', content: 'article' },
+      { hid: 'og:url', property: 'og:url', content: 'https://omaemona.info/article/3' },
       { hid: 'og:title', property: 'og:title', content: 'オマエモナー' },
       { hid: 'og:description', property: 'og:description', content: '記事を書いてMONAがもらえる！気に入った記事にMONAが送れる！' },
       { hid: 'og:image', property: 'og:image', content: 'https://omaemona.info:8443/ogp/default.png' },
-
+      // for twitter
+      { hid: 'twitter:creator', name: 'twitter:creator', content: '@Mr_1484'},
       { hid: 'twitter:card', name: 'twitter:card', content: 'summary_large_image' },
-      { hid: 'twitter:site', name: 'twitter:site', content: 'オマエモナー' },
-      { hid: 'twitter:title', property: 'twitter:title', content: 'オマエモナー' },
+      { hid: 'twitter:site', name: 'twitter:site', content: '@オマエモナー' },
+      { hid: 'twitter:title', name: 'twitter:title', content: 'オマエモナー' },
       { hid: 'twitter:description', name: 'twitter:description', content: '記事を書いてMONAがもらえる！気に入った記事にMONAが送れる！'},
       { hid: 'twitter:image', name: 'twitter:image', content: 'https://omaemona.info:8443/ogp/default.png'},
     ],
