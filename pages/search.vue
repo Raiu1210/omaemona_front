f96904914c5bdbbd6ef2180706833cd936f10ecb
Raiu1210 <baio1484@gmail.com>
2021-03-20 22:37:13 +0900

    added search

---
+++ pages/search.vue
@@ -0,0 +1,182 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-col
+        cols="12"
+        sm="1"
+      >
+        <v-sheet
+          color="#f5f5f5"
+          rounded="lg"
+          min-height="2vh"
+        >
+        </v-sheet>
+      </v-col>
+
+      <v-col cols="12" sm="8">
+        <v-sheet
+          class="sheet"
+          rounded="lg"
+        >
+          <v-row
+            v-for="article in articles"
+            :key="article.id"
+          >
+          <v-col>
+            <v-card
+              class="mx-auto"
+              color="#ffffff"
+              :to="{path : '/article/' + article.id}"
+            >
+              <v-card-subtitle class="pb-0 mb-0">
+                {{covertUpdateTime(article.updatedAt)}}
+              </v-card-subtitle>
+
+              <v-card-title class="pa-0 mt-2">
+                <span class="title font-weight-light"></span>
+              </v-card-title>
+
+              <v-card-text class="headline font-weight-bold mb-0 pb-0">
+                {{article.title}}
+              </v-card-text>
+
+              <v-card-actions>
+                <v-list-item class="grow">
+                  <v-list-item-avatar color="grey darken-3">
+                    <v-img
+                      class="elevation-6"
+                      alt=""
+                      :src="iconImagePath(article.user.icon_image_path)"
+                    ></v-img>
+                  </v-list-item-avatar>
+
+                  <v-list-item-content>
+                    <v-list-item-title>{{article.user.name}}</v-list-item-title>
+                  </v-list-item-content>
+
+                  <v-row
+                    align="center"
+                    justify="end"
+                  >
+                    <v-icon class="mr-1">
+                      mdi-alpha-m-circle-outline
+                    </v-icon>
+                    <span class="subheading mr-2">{{article.sent_mona.toFixed(8)}} MONA</span>
+                  </v-row>
+                </v-list-item>
+              </v-card-actions>
+            </v-card>
+            </v-col>
+          </v-row>
+
+          <v-row>
+            <v-col style="justify-content: center;">
+            <v-pagination
+              v-model="page"
+              :length="pageLength"
+              :total-visible="7"
+              @input = "gotoPageN"
+            ></v-pagination>
+            </v-col>
+          </v-row>
+        </v-sheet>
+      </v-col>
+
+      <v-col
+        cols="12"
+        sm="3"
+      >
+        <v-sheet
+          color="#f5f5f5"
+          rounded="lg"
+          min-height="600px"
+        >
+          <div v-html="adCode1"></div>
+          <div v-html="adCode2"></div>
+        </v-sheet>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+
+<script>
+import {axiosInstance as Api} from '~/myModules/api'
+
+export default {
+  data() {
+    return {
+      articles: [],
+      updated: '',
+      page: 1,
+      pageLength: 0,
+      tabIndex: 0,
+      destination: '',
+      adCode1: '<a href="https://px.a8.net/svt/ejp?a8mat=3H7UZP+CR186Q+348+1C6TPD" rel="nofollow"><img border="0" width="300" height="250" alt="" src="https://www23.a8.net/svt/bgt?aid=210318757771&wid=001&eno=01&mid=s00000000404008094000&mc=1"></a><img border="0" width="1" height="1" src="https://www10.a8.net/0.gif?a8mat=3H7UZP+CR186Q+348+1C6TPD" alt="">',
+      adCode2: '<a href="https://px.a8.net/svt/ejp?a8mat=3H7UZP+BKRG8I+50+4YTR9D" rel="nofollow"><img border="0" width="300" height="250" alt="" src="https://www26.a8.net/svt/bgt?aid=210318757700&wid=001&eno=01&mid=s00000000018030036000&mc=1"></a><img border="0" width="1" height="1" src="https://www12.a8.net/0.gif?a8mat=3H7UZP+BKRG8I+50+4YTR9D" alt="">'
+    }
+  },
+  async created() {
+    const res = await Api.get('searchArticle', {
+      params: {
+        q: this.$route.query.q,
+        page: this.$route.params.page == undefined ? 1 : this.$route.params.page
+      }
+    })
+    this.articles = res["data"]["articles"]
+    const articlesCount = res["data"]["articlesCount"]
+    this.pageLength = Math.ceil(articlesCount / 10)
+  },
+  methods: {
+    covertUpdateTime(timeData) {
+      const timeObj = new Date(timeData)
+      return timeObj.getFullYear() + '年' + (Number(timeObj.getMonth()) + 1) + '月' + timeObj.getDate() + '日'
+    },
+    gotoPageN(page) {
+      this.$router.push({ path: '/search',
+        query: {
+          q: this.$route.query.q,
+          page: page
+        }
+      })
+    },
+    iconImagePath(iconImagePath) {
+      const env = process.env.NODE_ENV || 'development'
+      let url = 'https://monaledge.com:8443'
+      if(env == 'development') {
+        url = 'http://localhost:3333'
+      }
+
+      if(iconImagePath == null) {
+        return url + '/profileImages/Monacoin.png'
+      } else {
+        return iconImagePath
+      }
+    }
+  },
+  async watchQuery(newQuery, oldQuery) {
+    const res = await Api.get('searchArticle', {
+      params: {
+        q: newQuery.q,
+        page: newQuery.page == undefined ? 1 : newQuery.page
+      }
+    })
+    console.log(res)
+    this.articles = res["data"]["articles"]
+    const articlesCount = res["data"]["articlesCount"]
+    this.pageLength = Math.ceil(articlesCount / 10)
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    })
+  },
+}
+</script>
+
+
+<style scoped>
+.sheet {
+  background:rgba(245,245,245,0);
+}
+
+</style>
